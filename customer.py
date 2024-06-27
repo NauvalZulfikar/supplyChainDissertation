0ea32d9a6c56e4711a20e1a7c5a147726c67da82
Nauval Zulfikar <70472247+NauvalZulfikar@users.noreply.github.com>
2024-06-27 18:36:55 +0100

    customer.py

--- customer.py
+++ customer.py
@@ -10,7 +10,7 @@ def customer(size):
     month          = [cust_ordDate[i].month for i in range(len(cust_ordDate))]
     cust_id        = [f'cust_{i}' for i in range(1,size+1)] # hashed
     cust_req_id    = [f'cust_req_{i}' for i in range(1,size+1)] # hashed
-    prod_name      = [f'product_{i}' for i in np.random.randint(1, 76, size=size)]
+    prod_name      = [f'product_{i}' for i in np.random.randint(1, 26, size=size)]
     cust_quan_req  = [i for i in np.random.randint(0,5000, size=size)]
     cust_price_ord = [i for i in np.random.uniform(0,2.5, size=size)]
     cust_duedates  = [pd.to_datetime(datetime.date.fromordinal(i.date().toordinal() + np.random.randint(6,7*2))) for i in cust_ordDate]
